c94a37587069526c827a84121c46bddc8d714653
vvnsreddy <vvnsreddy@gmail.com>
2024-03-27 23:18:34 -0700

    update

--- test.py
+++ test.py
@@ -1,117 +1,26 @@
-import pandas as pd
-import configparser
-import oandapyV20
-import oandapyV20.endpoints.orders as orders
-import oandapyV20.endpoints.positions as positions
-from datetime import date
-from flask import Flask, request, jsonify, render_template, Response
-from tda import auth, client
-from td.client import TDClient
-import os, json, datetime, math
-import os, json, requests
-import numpy as np
-import pandas as pd
-import time, pytz
-import datetime
-from datetime import datetime
-from pytz import timezone, utc
+# Define the slope of the line segment
+slope = 1
 
-now = datetime.now()
-today5am = now.replace(hour=13, minute=0, second=0, microsecond=0)
-today1pm = now.replace(hour=13 + 7, minute=0, second=0, microsecond=0)
+# To make it a valid density curve, the area under the curve must equal 1
+# Area of a triangle = 0.5 * base * height
+# Area = 0.5 * right_endpoint * right_endpoint = 1
+# Solve for the right_endpoint
+right_endpoint = 2
 
-if now > today5am and now < today1pm:
-    daytime = 1
-    daytime_multiplier = 4
-else:
-    daytime = 0
-    daytime_multiplier = 1
+# Percentage of values below 1
+percent_below_1 = (1 / 2) * 100  # Area of the triangle formed by the line segment and x-axis
 
+# Median
+median = right_endpoint / 2
 
-    print("DayTime:", daytime)
+# Q1
+Q1 = right_endpoint / 4
 
+# Q3
+Q3 = 3 * right_endpoint / 4
 
-DEBUG = 0
-
-
-def get_pst_time():
-    date_format='%m_%d_%Y_%H_%M_%S_%Z'
-    date = datetime.now(tz=pytz.utc)
-    date = date.astimezone(timezone('US/Pacific'))
-    pstDateTime=date.strftime(date_format)
-    return pstDateTime
-
-
-config = configparser.ConfigParser()
-config.read('./config.ini')
-accountID = config['oanda']['account_id']
-access_token = config['oanda']['api_key']
-app = Flask(__name__)
-client = oandapyV20.API(access_token=access_token)
-
-
-def ONADA_FOREX_ORDER(ticker, order_type, qty, price, position_type, exchange):
-    print(str(ticker))
-    if 'BUY_TO_OPEN' in str(order_type):
-        data = {
-            "order": {
-                "instrument": "EUR_USD",
-                "units": 1000,
-                "type": "MARKET",
-                "price": round(float(price),4),
-                "positionFill": "DEFAULT"
-            }
-        }
-        r = orders.OrderCreate(accountID, data=data)
-        client.request(r)
-        print(r)
-    elif 'SELL_TO_OPEN' in str(order_type):
-        data = {
-            "order": {
-                "instrument": "EUR_USD",
-                "units": -1000,
-                "type": "LIMIT",
-                "price": round(float(price),4),
-                "positionFill": "DEFAULT"
-            }
-        }
-        r = orders.OrderCreate(accountID, data=data)
-        client.request(r)
-    elif 'SELL_TO_CLOSE' in str(order_type):
-        print("SELL_TO_CLOSE")
-        ONADA_FOREX_CLOSE_POSITIONS()
-    elif 'BUY_TO_CLOSE' in str(order_type):
-        print("BUY_TO_CLOSE")
-        ONADA_FOREX_CLOSE_POSITIONS()
-
-
-def ONADA_FOREX_CLOSE_POSITIONS():
-    r = positions.OpenPositions(accountID=accountID)
-    client.request(r)
-    print(client.request(r))
-    if client.request(r)['positions']:
-        print(client.request(r)['positions'][0]['long']['units'])
-    data_long = {
-        "longUnits": "ALL"
-    }
-
-    data_short = {
-        "shortUnits": "ALL"
-    }
-    # r = positions.PositionClose(accountID=accountID,instrument='EUR_USD',data=data_long)
-    # r = positions.PositionClose(accountID=accountID,instrument='EUR_USD',data=data_short)
-
-    if client.request(r)['positions']:
-        if int(client.request(r)['positions'][0]['long']['units']) != 0:
-            rv = positions.PositionClose(accountID=accountID, instrument='EUR_USD', data=data_long)
-        elif int(client.request(r)['positions'][0]['short']['units']) != 0:
-            rv = positions.PositionClose(accountID=accountID, instrument='EUR_USD', data=data_short)
-        else:
-            rv = 'no orders executed'
-        client.request(rv)
-        print(rv.data)
-
-#ONADA_FOREX_ORDER(ticker, order_type, qty, price, position_type, exchange):
-
-
-ONADA_FOREX_ORDER("EURUSD", "BUY_TO_OPEN", 1000, 1.9, 459977, "ONADA")
+print("Right-endpoint:", right_endpoint)
+print("Percentage of values below 1:", percent_below_1, "%")
+print("Median:", median)
+print("Q1:", Q1)
+print("Q3:", Q3)
